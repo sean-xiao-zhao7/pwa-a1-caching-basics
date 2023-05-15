c12ae3d8051ae1381126ede889c1191972e87918
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2023-05-15 13:46:01 -0400

    Add service worker

---
+++ public/service-worker.js
@@ -0,0 +1,8 @@
+// service worker install
+self.addEventListener("install", (event) => {});
+
+// activate
+self.addEventListener("activate", (event) => {});
+
+// fetch
+self.addEventListener("fetch", (event) => {});
